42008e1266d64019ea3dd75d5e4769fcf3d765f4
jayouimet <jayouimet@hotmail.com>
2025-03-21 19:43:50 -0400

    GLFW and Basic Window

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,11 @@ workspace "Hazel"
 	}
 	
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
+	
+include "Hazel/vendor/GLFW"
 	
 project "Hazel"
 	location "Hazel"
@@ -33,7 +38,14 @@ project "Hazel"
 	includedirs
 	{
 		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include"
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+	
+	links
+	{
+		"GLFW",
+		"opengl32.lib"
 	}
 		
 	buildoptions { "/utf-8" }
