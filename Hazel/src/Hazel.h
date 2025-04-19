5606b230b155bbccf15730f98a6c2e3eddde0cc8
jayouimet <jayouimet@hotmail.com>
2025-04-19 19:27:41 -0400

    Orthographic camera and movement test

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -13,5 +13,15 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+// Renderer
+#include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/RenderCommand.h"
+
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/VertexArray.h"
+
+#include "Hazel/Renderer/OrthographicCamera.h"
+
 // Entry point
 #include "Hazel/EntryPoint.h"
